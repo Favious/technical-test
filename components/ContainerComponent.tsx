7d99e26e27ade661a2fc56cf42b5bb0fe52430ac
Favious <faviopaco@gmail.com>
2023-01-22 17:50:05 -0400

    isolate home page and add buttons to it

--- components/ContainerComponent.tsx
+++ components/ContainerComponent.tsx
@@ -1,62 +1,15 @@
-import styles from "@/styles/Home.module.css";
+import React from "react";
 import styled from "styled-components";
+import { Aclonica } from "@next/font/google";
+import { Andada_Pro } from "@next/font/google";
 
-import { FaGithub } from "react-icons/fa";
+const aclonica = Aclonica({ weight: "400", subsets: ["latin"] });
+const andadaPro = Andada_Pro({ weight: "400", subsets: ["latin"] });
 
-export default function ContainerComponent() {
+export default function ContainerComponent({ children }: { children: any }) {
   return (
-    <Container>
-      <div className="title">
-        <h1>HELLO</h1>
-      </div>
-      <div className="content">
-        <div>
-          This is a technical test solution made by Favio Pariente in order to
-          search repositores and users from:
-        </div>
-        <div className="gitHub">
-          <FaGithub />
-          GitHub
-        </div>
-      </div>
-    </Container>
+    <>
+      {React.cloneElement(children, { ...children.props, aclonica, andadaPro })}
+    </>
   );
 }
-
-const Container = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-  height: 100vh;
-  width: 100%;
-  padding-bottom: 25vh;
-  .title {
-    margin-bottom: 1.5rem;
-  }
-  .title h1 {
-    color: var(--cian);
-    font-size: 5rem;
-    position: relative;
-  }
-  .content {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    color: var(--silver);
-    font-size: 2rem;
-    width: 60%;
-    text-align: center;
-
-    .gitHub {
-      flex-direction: column;
-      font-size: 1rem;
-      margin: 2rem;
-      svg {
-        color: var(--lightWhite);
-        height: 4rem;
-        width: 5rem;
-      }
-    }
-  }
-`;
---
+++ pages/HomePage.tsx
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
+
+export default function HomePage({
+  aclonica,
+  andadaPro,
+}: {
+  aclonica: any;
+  andadaPro: any;
+}) {
+  return (
+    <Section>
+      <div className="title">
+        <h1 className={aclonica.className}>HELLO</h1>
+      </div>
+      <div className="content">
+        <div className={andadaPro.className}>
+          This is a technical test solution made by Favio Pariente in order to
+          search repositores and users from:
+        </div>
+        <div className="gitHub">
+          <FaGithub />
+          GitHub
+        </div>
+      </div>
+      <div className="buttons-row">
+        <div className="element">
+          <button className={andadaPro.className}>FIND USERS</button>
+        </div>
+        <div className="element">
+          <button className={andadaPro.className}>FIND REPOS</button>
+        </div>
+      </div>
+    </Section>
+  );
+}
+
+const Section = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  padding-bottom: 5vh;
+  .title {
+    margin-bottom: 0.8rem;
+  }
+  .title h1 {
+    color: var(--cian);
+    font-size: 5rem;
+    position: relative;
+  }
+  .content {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    color: var(--silver);
+    font-size: 2rem;
+    width: 58%;
+    text-align: center;
+
+    .gitHub {
+      flex-direction: column;
+      font-size: 1rem;
+      margin: 2rem;
+      svg {
+        color: var(--lightWhite);
+        height: 4rem;
+        width: 5rem;
+      }
+    }
+  }
+  .buttons-row {
+    display: flex;
+    gap: 4rem;
+    flex-direction: row;
+    margin-top: 10vh;
+    .element {
+      button {
+        padding: 0.8rem 1.2rem;
+        font-size: 1rem;
+        background-color: transparent;
+        color: var(--lightWhite);
+        border-radius: 0.4rem;
+        border: 2px solid var(--cian);
+        &:hover {
+          background-color: var(--cian);
+          color: var(--darkBlue);
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`;
